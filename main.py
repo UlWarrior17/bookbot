dc5410dc213537ada35cbde4f43b5ddfd81d1b23
UlWarrior17 <ulrichneud@gmail.com>
2025-10-16 22:55:26 +0100

    print report

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from stats import get_num_words, char_count
+from stats import get_num_words, get_num_charaters, get_charaters_list
 
 
 def main():
     path = "books/frankenstein.txt"
-    result = get_book_text(path)
+    text = get_book_text(path)
+    word_count = get_num_words(text)
+    char_count_dict = get_num_charaters(text)
+    char_count_list = get_charaters_list(char_count_dict)
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {path}...')
+    print('----------- Word Count ----------')
+    print(f'Found {word_count} total words')
+    print('--------- Character Count -------')
+    for char_dict in char_count_list:
+        if char_dict['char'].isalpha():
+            print(f'{char_dict['char']}: {char_dict['num']}')
+    print('============= END ===============')
 
-    num_words = get_num_words(result)
-    print(f"Found {num_words} total words")
-    char_count(result)
     
 
 def get_book_text(filepath):
--- stats.py
+++ stats.py
@@ -2,13 +2,23 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def char_count(text):
+def get_num_charaters(text):
     char_dict = {}
-    # print(text.lower())
     for char in text.lower():
         if char not in char_dict:
             char_dict[char] = 1
         else:
             char_dict[char] += 1
            
-    print(char_dict)
+    return char_dict
+
+def get_charaters_list(char_dict):
+    dict_list = []
+    for char in char_dict:
+        dict_list.append({'char': char, 'num': char_dict[char]})
+
+    def get_num(dictionary):
+        return dictionary['num']
+
+    dict_list.sort(key=get_num, reverse=True)
+    return dict_list
